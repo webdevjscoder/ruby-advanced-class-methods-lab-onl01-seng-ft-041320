690ea6394d3334d1bfa5d17fe9e89ac35aff5e7f
Juan Cobian <cobianjuan19@gmail.com>
2020-04-21 22:19:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,7 +49,9 @@ class Song
   
   def self.new_from_filename(filename)
     song_array = filename.split(' - ')
-    song_array[1] = song_array[1].chomp()
+    song_array[1] = song_array[1].chomp('.mp3')
+    song = self.new
+    song
   end
   
   def self.create_from_filename(filename)
