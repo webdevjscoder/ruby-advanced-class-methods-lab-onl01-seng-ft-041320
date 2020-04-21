b65eb66b6b16974ca81f5cfa2d255863196dc345
Juan Cobian <cobianjuan19@gmail.com>
2020-04-21 22:21:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,7 +52,7 @@ class Song
     song_array[1] = song_array[1].chomp('.mp3')
     song = self.new
     song.name = song_array[1]
-    song,artist_name = song_array[0]
+    song.artist_name = song_array[0]
     song
   end
   
