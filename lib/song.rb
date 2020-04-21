e21af5473ceab84e0bd1d76de549400709595509
Juan Cobian <cobianjuan19@gmail.com>
2020-04-21 22:22:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -57,7 +57,12 @@ class Song
   end
   
   def self.create_from_filename(filename)
-    
+    song_array = filename.split(' - ')
+    song_array[1] = song_array[1].chomp('.mp3')
+    song = self.new
+    song.name = song_array[1]
+    song.artist_name = song_array[0]
+    self.all << song
   end
   
   def self.destroy_all
