6f392ad2d6fe2fc4545b06ccb6aef7734ac82066
Juan Cobian <cobianjuan19@gmail.com>
2020-04-21 21:58:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,7 @@ class Song
   end
   
   def self.find_by_name(title)
-    result = self.all.find_by_name { |song| song.name == name }
+    result = self.all. { |song| song.name == name }
     result
   end
 
